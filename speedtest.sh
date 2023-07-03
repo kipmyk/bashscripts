c34ce62bd3941ae50563a6cbe5dacd98f403a681
kipmyk <kipmyk@gmail.com>
2023-07-03 15:06:22 +0300

    update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,61 +1,58 @@
 #!/usr/bin/env bash
 ########################################################################
-# Author: Joseph Nix nixjdm@terminallabs.com 6-18-2015
-# Copyright (c) 2015 Terminal Labs
-# Distributed under the MIT License.
-# (See http://opensource.org/licenses/MIT)
+# Author: Kipmyk
+# Date: 2023-07-03
+# Description: This script tests the average page load time for a given URL. It can be set to run any number of tests and then averages the results.
 #
-# This tests the average page load time for a given url. It can be set
-# to run any number of tests, and then will average the results.
-#
-# Takes the url as the first arg, and the number of tests as the
-# second arg
-#
-# Example usage: ./speedtest.sh www.mysite.com 20
+# Usage: ./speedtest.sh <url> <iterations>
+# Example: ./speedtest.sh www.example.com 20
 ########################################################################
 
 url=$1
 iterations=$2
 times=""
-echo testing load times for $url
-for i in `seq 1 $iterations`
-    do
-        result=`wget -p $url 2>&1 | tail -n2 | head -n1`
-	prefix="Total wall clock time: "
-        time=${result#$prefix}
-	if [[ $time == *m* ]] #convert Xm Ys times and Xs times to just int seconds
-	    then
-	        minutes=`echo $time | awk '{print $1}' | awk -F "m" '{print $1}'`
-		seconds=`echo $time | awk '{print $2}' | awk -F "s" '{print $1}'`
-		time=$(($minutes*60+$seconds)) #assumes int seconds, unlike if wget results are under 10s
-	    else
-	        time=`echo $time | awk -F "s" '{print $1}'` #may be int or float, doesn't matter
-	fi
-        if [ "$times" == "" ] #first pass, no times yet
-	    then
-	        times=$time
-	    else #every other pass, add a space and then the next time
-	        times="$times $time"
-	fi
-	echo test $i loaded in $time seconds
+
+echo "Testing load times for $url"
+
+for ((i = 1; i <= $iterations; i++)); do
+    result=$(wget -p $url 2>&1 | tail -n2 | head -n1)
+    prefix="Total wall clock time: "
+    time=${result#$prefix}
+
+    if [[ $time == *m* ]]; then
+        minutes=$(echo $time | awk '{print $1}' | awk -F "m" '{print $1}')
+        seconds=$(echo $time | awk '{print $2}' | awk -F "s" '{print $1}')
+        time=$(($minutes * 60 + $seconds))
+    else
+        time=$(echo $time | awk -F "s" '{print $1}')
+    fi
+
+    if [ "$times" == "" ]; then
+        times=$time
+    else
+        times="$times $time"
+    fi
+
+    echo "Test $i loaded in $time seconds"
 done
 
-##Results
-echo There were $iterations test page loads of $url
+# Results
+echo "There were $iterations page load tests for $url"
+
 echo $times | awk '{
     min = max = sum = $1;
     sum_of_squares = $1 * $1;
-    for (n=2; n <= NF; n++) {
+    for (n = 2; n <= NF; n++) {
         if ($n < min) min = $n;
         if ($n > max) max = $n;
         sum += $n;
         sum_of_squares += $n * $n;
     }
-    avg = sum/NF;
+    avg = sum / NF;
     sum2 = 0;
-    for (n=1; n <= NF; n++) {
-        sum2 += ($n-avg)^2;
+    for (n = 1; n <= NF; n++) {
+        sum2 += ($n - avg) ^ 2;
     }
-    stddev = sqrt(sum2/NF);
-    print "All units in seconds: min=" min ", max=" max ", avg=" sum/NF ", sum=" sum ", standard deviation=" stddev;
+    stddev = sqrt(sum2 / NF);
+    print "All values are in seconds: minimum=" min ", maximum=" max ", average=" avg ", sum=" sum ", standard deviation=" stddev;
 }'
