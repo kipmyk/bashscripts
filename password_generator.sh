3adca6c777b158aae6d82b4b15a78c7ca5d12ae8
kipmyk <kipmyk@gmail.com>
2023-07-04 10:10:02 +0300

    added use case

--- password_generator.sh
+++ password_generator.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+########################################################################
+# Password generator script
+# Author: Kipmyk
+# Date: 2023-07-04
+# Description: This script tests the average page load time for a given URL. It can be set to run any number of tests and then averages the results.
+#
+# Usage: To use this script, simply save it to a file (e.g., password_generator.sh), make it executable  (chmod +x password_generator.sh), and then run it (./password_generator.sh)
+# Example: ./password_generator.sh 
+########################################################################
 
 # Define the characters that can be used in the password
 CHARACTERS="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-="
